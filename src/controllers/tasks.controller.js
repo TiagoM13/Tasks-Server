7312f5c3490f545f28989f348ce631418e2ddcf9
TiagoM13 <thyagomotha2000@gmail.com>
2023-11-02 17:57:05 -0300

    feat: add new controllers

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,10 +1,29 @@
 const tasksModel = require('../models/tasks.model');
 
-const getAllTasks = async (req, res) => {
+const getAllTasks = async (_req, res) => {
   const tasks = await tasksModel.getAllTasks();
 
   return res.status(200).json(tasks);
 };
 
+const createTask = async (req, res) => {
+  const createTask = await tasksModel.createTask(req.body);
 
-module.exports = { getAllTasks };
+  return res.status(201).json(createTask);
+};
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  await tasksModel.deleteTask(id);
+  return res.status(204).json();
+};
+
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+
+  await tasksModel.updateTask(id, req.body);
+  return res.status(204).json();
+};
+
+module.exports = { getAllTasks, createTask, deleteTask, updateTask };
